eeff214048d2400181d52c9e1aceb03c0a0ef28a
yqz66 <894528698@qq.com>
2018-08-20 09:55:46 +0800

    修改自定义负载均衡实现

--- my-consumer-dept-8003/src/main/java/my/consumer/config/MyIRule.java
+++ my-consumer-dept-8003/src/main/java/my/consumer/config/MyIRule.java
@@ -18,13 +18,10 @@ public class MyIRule extends AbstractLoadBalancerRule {
         ILoadBalancer loadBalancer = this.getLoadBalancer();
         List<Server> allServers = loadBalancer.getAllServers();
         Random random = new Random();
-        int i = random.nextInt(10);
+        int i = random.nextInt(allServers.size());
         System.out.println("自定义负载均衡");
-        // i 小于 3 使用第一个 否则使用第二个 (这里一共起了两个服务，可以根据自己的需求，IP 端口 之类的自己实现负载均衡)
-        if (i < 3) {
-            return allServers.get(0);
-        }
-        return allServers.get(1);
+        // 随机返回服务
+        return allServers.get(i);
         //for (Server client : allServers) {
         //    System.out.println(client.getHostPort());
         //}
